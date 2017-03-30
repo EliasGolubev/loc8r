f14bcfe507a435eec7d028dc5ffbcd83d0d60d52
Elias <igolubevssmnu@mail.ru>
2017-03-31 01:00:47 +0300

    Fixed

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+if (process.env.MONGODB_URI === undefined){
+  require('./secrets/env.js');
+}
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
