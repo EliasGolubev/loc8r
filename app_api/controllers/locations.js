0c174254efcb0cc7df06830e52ef1ffd215f57ec
Elias <igolubevssmnu@mail.ru>
2017-04-01 16:01:54 +0300

    Model api

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -4,12 +4,25 @@ var sendJsonResponce = function(res, status, content){
   res.json(content);
 };
 
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
 module.exports.locationsListByDistance = function(req, res){ 
   sendJsonResponce(res, 200, {"status" : "success"});
 };
 
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
 module.exports.locationsCreate= function(req, res){ /* . . . */ };
+
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
 module.exports.locationsReadOne = function(req, res){ /* . . . */ };
+
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
 module.exports.locationsUpdateOne = function(req, res){ /* . . . */ };
+
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
 module.exports.locationsDeleteOne = function(req, res){ /* . . . */ };
 
